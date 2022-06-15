b3b8ad88c7c494e75f7516a42b63cdb1cc44ad3e
Rodrigo Gonçalves  - BR Consórcios - Sistemas <rodrigo.goncalves@consorciouniaoldb.com.br>
2022-06-15 17:50:40 -0300

    Fim da quinta aula

---
+++ src/bytebank/TestaClasse.java
@@ -0,0 +1,11 @@
+package bytebank;
+
+public class TestaClasse {
+	public static void main(String[] args) {
+		Conta conta = new Conta();
+		conta.saldo = 100;
+		conta.deposita(100);
+		
+		System.out.println("Saldo conta: " + conta.saldo);
+	}
+}
